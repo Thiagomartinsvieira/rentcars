8846250846e6bf4e60f303f369a1a22b3e83a9f7
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-10-15 20:11:40 -0300

    feat(login-options): implement login options page with design improvements and registration link

---
+++ frontend/src/app/pages/login-options/login-options.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login-options',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './login-options.component.html',
+  styleUrl: './login-options.component.scss'
+})
+export class LoginOptionsComponent {
+
+}
