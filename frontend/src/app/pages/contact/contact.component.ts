662a71a43c4d699d964397125021912dae267300
Thiago Martins Vieira <thiagoalagadico@gmail.com>
2024-10-16 21:24:13 -0300

    feat: add Help Center (contact) page with useful information and inquiry form

--- frontend/src/app/pages/contact/contact.component.ts
+++ frontend/src/app/pages/contact/contact.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  name: string = "";
+  email: string = "";
+  message: string = "";
 
+  constructor() {}
+
+  submitForm() {
+    if(this.name && this.email && this.message) {
+      const userMessage = {
+        name: this.name,
+        email: this.email,
+        message: this.message,
+      }
+
+      console.log("Form data:", userMessage);
+
+      alert("Your Question has been sent successfully! We Will contact you shortly");
+
+      this.name = "";
+      this.email = "";
+      this.message = "";
+    } else {
+      alert("Please, fill in all the fields of the form")
+    }
+  }
 }
